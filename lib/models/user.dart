3be119ca3346bfd45b0257eccbeaa443e98b464f
cacausilveira <carolina.silveira92@gmail.com>
2021-02-03 16:31:35 -0300

    Initial commit

---
+++ lib/models/user.dart
@@ -0,0 +1,21 @@
+import 'package:social_app/models/address.dart';
+
+class User {
+  int id;
+  String name;
+  String username;
+  String email;
+  Address address;
+  String phone;
+  String website;
+
+  User.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        username = json['username'],
+        email = json['email'],
+        address = Address.fromJson(json['zipcode']),
+        phone = json['phone'],
+        website = json['website'];
+
+}
